b26212b2a05ff09f1862617c16df2349ca8e7ea2
Alex Flynn <alex@thorflyn.com>
2016-02-26 23:51:08 -0500

    Update FlamingPigs.java
    Wanted to match the original code closer to see differences.

--- src/main/java/org/devoxx4kids/forge/mods/FlamingPigs.java
+++ src/main/java/org/devoxx4kids/forge/mods/FlamingPigs.java
@@ -16,14 +16,14 @@ public class FlamingPigs implements ICommand {
 
 	private List aliases = new ArrayList();
 	private int numberOfPigs = 0;
-
-	public FlamingPigs() {
+	
+	public FlamingPigs(){
 		aliases.add("flamingpigs");
 		aliases.add("fp");
 	}
 
 	@Override
-	public int compareTo(Object o) {
+	public int compareTo(ICommand arg0) {
 		return 0;
 	}
 
@@ -32,61 +32,55 @@ public class FlamingPigs implements ICommand {
 		return "/flamingpigs <number of pigs>";
 	}
 
-	@Override
-	public boolean isUsernameIndex(String[] args, int index) {
+	public boolean isUsernameIndex(String[] astring, int i){
 		return false;
 	}
 
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+	public String getCommandName(){
+		return "flamingpigs";
 	}
-
+	
 	@Override
-	public List getAliases() {
+	public List<String> getCommandAliases() {
 		return aliases;
 	}
-
+	
 	@Override
-	public void execute(ICommandSender sender, String[] args)
-			throws CommandException {
-		if (args.length != 1) {
+	public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+		if (args.length != 1){
 			sendErrorMessage(sender, "Invalid number of arguments!");
 			return;
 		}
-		try {
+		try{
 			numberOfPigs = Integer.parseInt(args[0]);
-		} catch (NumberFormatException e) {
-			sendErrorMessage(sender, "The argument \"" + args[0]
-					+ "\" is not a valid number!");
+		} catch(NumberFormatException e){
+			sendErrorMessage(sender, "The argument \"" + args[0] + "\" is not a valid number!");
 			return;
 		}
 		EntityPlayer player = (EntityPlayer) sender;
-		for (int i = 0; i < numberOfPigs; i++) {
+		for (int i = 0; i < numberOfPigs; i++){
 			EntityPig pig = new EntityPig(player.worldObj);
-			pig.setLocationAndAngles(player.posX, player.posY, player.posZ, 0,
-					0);
+			pig.setLocationAndAngles(player.posX, player.posY, player.posZ, 0,0);
 			pig.setFire(10000);
 			player.worldObj.spawnEntityInWorld(pig);
-		}
+		}		
 	}
-
-	private void sendErrorMessage(ICommandSender sender, String message) {
-		sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_RED
-				+ message));
+	
+	public boolean canCommandSenderUseCommand(ICommandSender icommandsender){
+	  return true;
 	}
-
-	@Override
-	public boolean canCommandSenderUse(ICommandSender sender) {
-		return sender instanceof EntityPlayer;
-	}
-
-	@Override
-	public List addTabCompletionOptions(ICommandSender sender, String[] args,
-			BlockPos pos) {
-		// TODO Auto-generated method stub
+	
+	public List addTabCompletionOptions(ICommandSender icommandsender, String[] astring){
 		return null;
 	}
-
+	
+	public void sendErrorMessage(ICommandSender sender, String message){
+		sender.addChatMessage(new ChatComponentText(EnumChatFormatting.DARK_RED + message));
+	}
+	
+	@Override
+	public List<String> addTabCompletionOptions(ICommandSender sender, String[] args, BlockPos pos) {
+		return null;
+	}
+	
 }
